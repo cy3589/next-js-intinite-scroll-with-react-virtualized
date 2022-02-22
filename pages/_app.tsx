42092c2f5e6feb5ad86b8e9aa5eb24d9cb797f6c
정찬영 <jcy3589@gmail.com>
2022-02-22 17:39:18 +0000

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+// import { dehydrate } from 'react-query/hydration';
+import { useRef } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) queryClientRef.current = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
